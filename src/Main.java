de8ac36c55df0e9678cbf17f0065df0cce432d8c
Bryan Lam <bryanlam@Bryans-MacBook-Pro.local>
2014-02-11 19:50:06 -0800

    Added binary search

--- src/Main.java
+++ src/Main.java
@@ -18,10 +18,55 @@ public class Main {
 		MergeSort ms = new MergeSort();
 		QuickSort qs = new QuickSort();
 		test= ms.mergeSort(test);
-		for (int i=0; i<test.size(); i++)
+//		for (int i=0; i<test.size(); i++)
+//		{
+//			System.out.println(test.get(i));
+//		}
+		
+		System.out.println(binarySearch(test, 10));
+		
+	}
+	//array must already be sorted
+	public static boolean binarySearch(ArrayList<Integer> s, int x)
+	{
+		//For later
+		ArrayList<Integer> sub = new ArrayList<Integer>();
+		//corner cases
+		if (s.size() == 0)
 		{
-			System.out.println(test.get(i));
+			return false;
 		}
+		
+		else
+		{
+			//If its equal to the middle then return true
+			int middle = s.size()/2;
+			if(s.get(middle) == x)
+			{
+				return true;
+			}
+			else
+			{
+				if(x<s.get(middle))
+				{
+					//up to not including middle
+					for (int i =0; i<middle; i++)
+					{
+						sub.add(s.get(i));
+					}
+				}
+				else
+				{
+					//everything after middle
+					for (int i= middle+1; i<s.size(); i++)
+					{
+						sub.add(s.get(i));
+					}
+				}
+				return binarySearch(sub, x);
+			}
+		}
+		
 	}
 
 }
